410529c5f2a3ec7d3bd8b900df4af1b0353d5a0e
anastasia <anastasiiadesiateryk@gmail.com>
2025-04-07 13:57:36 +0300

    changed cors origins

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ if not UNSPLASH_KEY:
 
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+# CORS(app, origins=["http://localhost:3000"])
+CORS(app, origins=["https://images-gallery-anastasiia.netlify.app"])
 
 app.config["DEBUG"] = DEBUG
 
